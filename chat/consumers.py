830fc7c6613b20dfb13a3ee09d1d1b4e49501bdd
Pawan Paudel <pawanpaudel93@gmail.com>
2021-01-27 00:03:29 +0545

    added vue and autoscroll chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -25,20 +25,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
         # send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'username': username,
+                'message': message,
             }
         )
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        username = event['username']
 
         # Send message to Websocket
         await self.send(text_data=json.dumps({
+            'username': username,
             'message': message
         }))
